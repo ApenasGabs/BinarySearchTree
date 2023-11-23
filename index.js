d22ee7372e32a6b1f9956949b2ddd5023ce7a388
Gabriel Rodrigues <gabers357@gmail.com>
2023-11-23 19:30:21 -0300

    Add Binary Search Tree implementation

--- index.js
+++ index.js
@@ -7,3 +7,33 @@ class Node {
       }
   }
 }
+
+// const MyNode = new Node('batata')
+
+// console.log('MyNode: ', MyNode);
+
+
+class BinarySearchTree{
+  constructor(){
+    this.root = null
+  }
+  insert(value){
+    if (this.root === null){
+      this.root = new Node(value)
+    } else{
+      if(value < this.root.value){
+        this.left = new Node(value)
+      }
+      if(value > this.root.value){
+        this.right = new Node(value)
+      }
+    }
+  }
+}
+
+const BTS = new BinarySearchTree()
+
+BTS.insert(2)
+BTS.insert(1)
+BTS.insert(3)
+console.log('BTS: ', BTS);
