aaae3331deff736894a97ee47365b0b83476ee88
Gabriel Rodrigues <gabers357@gmail.com>
2023-11-24 02:16:39 -0300

    Add Node struct and Insert method to main.go

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main()  {
-	
+type Node struct {
+	Left  *Node
+	Right *Node
+	Value int
+}
+
+func (root *Node) Insert(value int) {
+	if root == nil {
+		return
+	}
+	if root.Value < value {
+		if root.Left == nil {
+			root.Left = &Node{Value: value}
+		} else {
+			root.Left.Insert(value)
+		}
+	} else {
+		if root.Right == nil {
+			root.Right = &Node{Value: value}
+		} else {
+			root.Right.Insert(value)
+		}
+	}
+}
+
+func main() {
+
 }
